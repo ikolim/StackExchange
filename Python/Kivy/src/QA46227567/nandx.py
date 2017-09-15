4f545f971fbbfb0c6d1e98eee0c6f33fa8d67ea4
ikolim <daniel.kolim@gmail.com>
2017-09-15 11:09:45 -0600

    Add files via upload

---
+++ Python/Kivy/src/QA46227567/nandx.py
@@ -0,0 +1,39 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.config import Config
+
+Config.set("graphics", "width", "400")
+Config.set("graphics", "height", "570")
+Config.set("graphics", "borderless", "0")
+Config.set("graphics", "resizable", "0")
+Config.set("kivy", "window_icon", "nyc.ico")
+
+
+class Manager(ScreenManager):
+    pass
+
+
+class ScreenOne(Screen, Widget):
+    pass
+
+
+class ScreenTwo(Screen, Widget):
+
+    def methodforbuttonpress(self, button_pressed):
+        kivy_logo = "kivy-logo-black-64.png"
+        self.ids[button_pressed].background_normal = kivy_logo
+
+
+class ScreenThree(Screen):
+    pass
+
+
+class nandxApp(App):
+    def build(self):
+        self.title = "Noughts and Crosses"
+        return Manager()
+
+
+if __name__ == "__main__":
+    nandxApp().run()
